e7aee050c9cf65a391a461b39c467d180d446bc1
reesecantu <65140498+reesecantu@users.noreply.github.com>
2026-01-25 21:41:46 -0500

    redirect labs to Labs because I don't wanna deal with github file renaming nonsense

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  redirects: async () => {
+    return [
+      {
+        source: "/labs",
+        destination: "/Labs",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
