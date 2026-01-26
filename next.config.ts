5ea72e5a48aa1687545f2692c40eca370265d853
reesecantu <65140498+reesecantu@users.noreply.github.com>
2026-01-25 21:44:27 -0500

    nvm, I guess I'll deal with github file renaming

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  redirects: async () => {
-    return [
-      {
-        source: "/labs",
-        destination: "/Labs",
-        permanent: true,
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
