341f51b856d00f580b9a90f834b814e4c916d551
reesecantu <65140498+reesecantu@users.noreply.github.com>
2026-02-09 17:11:05 -0500

    2.4 completed!

--- app/Labs/Lab2/BootstrapNavigation.tsx
+++ app/Labs/Lab2/BootstrapNavigation.tsx
@@ -1,5 +1,4 @@
-import { Nav, NavItem, NavLink } from "react-bootstrap";
-
+import { Nav, NavItem, NavLink, Card, CardImg, CardBody, CardTitle, CardText, Button } from "react-bootstrap";
 
 export default function BootstrapNavigation() {
   return (
@@ -22,6 +21,19 @@ export default function BootstrapNavigation() {
             </NavLink>
           </NavItem>
         </Nav>
+      </div>
+      <div id="wd-css-navigating-with-cards">
+        <h2> Cards </h2>
+        <Card style={{ width: "18rem" }}>
+          <CardImg variant="top" src="/images/stacked.jpg" />
+          <CardBody>
+            <CardTitle>Stacking Starship</CardTitle>
+            <CardText>
+              Stacking the most powerful rocket in history. Mars or bust!
+            </CardText>
+            <Button variant="primary">Boldly Go</Button>
+          </CardBody>
+        </Card>
       </div>
     </div>
   );
