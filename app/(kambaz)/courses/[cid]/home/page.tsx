b496b836bb380d27a45e14a34954227cc90c46b0
reesecantu <65140498+reesecantu@users.noreply.github.com>
2026-02-10 00:12:35 -0500

    Reformat profile related pages with Bootstrap components

--- app/(kambaz)/courses/[cid]/home/page.tsx
+++ app/(kambaz)/courses/[cid]/home/page.tsx
@@ -1,15 +1,18 @@
+import { Row, Col } from "react-bootstrap";
 import Modules from "../modules/page";
 import CourseStatus from "./Status";
+
 export default function Home() {
- return (
-   <div id="wd-home">
-     <table>
-       <tbody>
-         <tr>
-           <td valign="top" width="70%"> <Modules />      </td>
-           <td valign="top">             <CourseStatus /> </td>
-         </tr>
-       </tbody>
-     </table>
-   </div>
-);}
+  return (
+    <div id="wd-home">
+      <Row>
+        <Col>
+          <Modules />
+        </Col>
+        <Col xl={4} className="d-none d-xl-block">
+          <CourseStatus />
+        </Col>
+      </Row>
+    </div>
+  );
+}
