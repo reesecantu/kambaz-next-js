7ee74b57565f483f080b3ad4b13055c7137ee374
reesecantu <65140498+reesecantu@users.noreply.github.com>
2026-02-09 22:47:26 -0500

    2.5.6

--- app/(kambaz)/courses/[cid]/home/Status.tsx
+++ app/(kambaz)/courses/[cid]/home/Status.tsx
@@ -1,8 +1,43 @@
+import { MdDoNotDisturbAlt } from "react-icons/md";
+import { FaCheckCircle } from "react-icons/fa";
+import { BiImport } from "react-icons/bi";
+import { LiaFileImportSolid } from "react-icons/lia";
+import { MdCalendarToday } from "react-icons/md";
+import { MdNotifications } from "react-icons/md";
+import { Button } from "react-bootstrap";
+{
+  /* Find more icons */
+}
 export default function CourseStatus() {
   return (
-    <div id="wd-course-status">
+    <div id="wd-course-status" style={{ width: "350px" }}>
       <h2>Course Status</h2>
-      <button>Unpublish</button> <button>Publish</button>
-      {/* Complete on your own */}
-      <button>View Course Notifications</button>
-    </div> );}
+      <div className="d-flex">
+        <div className="w-50 pe-1">
+          <Button variant="secondary" size="lg" className="w-100 text-nowrap ">
+            <MdDoNotDisturbAlt className="me-2 fs-5" /> Unpublish{" "}
+          </Button>{" "}
+        </div>
+        <div className="w-50">
+          <Button variant="success" size="lg" className="w-100">
+            <FaCheckCircle className="me-2 fs-5" /> Publish{" "}
+          </Button>{" "}
+        </div>
+      </div>
+      <br />
+      <Button variant="secondary" size="lg" className="w-100 mt-1 text-start">
+        <BiImport className="me-2 fs-5" /> Import Existing Content{" "}
+      </Button>
+      <Button variant="secondary" size="lg" className="w-100 mt-1 text-start">
+        <LiaFileImportSolid className="me-2 fs-5" /> Import from Commons{" "}
+      </Button>
+      <Button variant="secondary" size="lg" className="w-100 mt-1 text-start">
+        <MdCalendarToday className="me-2 fs-5" /> View Course Calendar
+      </Button>
+      <Button variant="secondary" size="lg" className="w-100 mt-1 text-start">
+        <MdNotifications className="me-2 fs-5" /> View Course Notifications
+      </Button>
+      {/* Complete the rest of the buttons */}
+    </div>
+  );
+}
