55d247d79071225da3f5eb2e03088abe95d84f05
reesecantu <65140498+reesecantu@users.noreply.github.com>
2026-03-16 21:55:14 -0400

    4.3.4 implementation

--- app/Labs/Lab4/store.ts
+++ app/Labs/Lab4/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import helloReducer from "./redux/hello/helloReducer";
 import counterReducer from "./redux/CounterRedux/counterReducer";
+import addReducer from "./redux/AddRedux/addReducer";
 
 const store = configureStore({
   reducer: {
     helloReducer,
     counterReducer,
+    addReducer,
   },
 });
 export type RootState = ReturnType<typeof store.getState>;
