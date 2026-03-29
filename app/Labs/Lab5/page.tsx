ccb9807e11c5d7cbc10d0821774e93e2e574e842
reesecantu <65140498+reesecantu@users.noreply.github.com>
2026-03-28 20:01:39 -0400

    Full 5.2 implementation

--- app/Labs/Lab5/page.tsx
+++ app/Labs/Lab5/page.tsx
@@ -1,22 +1,30 @@
 import EnvironmentVariables from "./EnvironmentVariables";
+import HttpClient from "./HttpClient";
 import PathParameters from "./PathParameters";
 import QueryParameters from "./QueryParameters";
 import WorkingWithArrays from "./WorkingWithArrays";
+import WorkingWithArraysAsynchronously from "./WorkingWithArraysAsyncronously";
 import WorkingWithObjects from "./WorkingWithObjects";
+import WorkingWithObjectsAsynchronously from "./WorkingWithObjectsAsynchronously";
 const HTTP_SERVER = process.env.NEXT_PUBLIC_HTTP_SERVER;
 export default function Lab5() {
   return (
-    <div id="wd-lab5">
+    <div id="wd-lab5" className="max-w-4xl px-5">
       <h2>Lab 5</h2>
       <div className="list-group">
         <a href={`${HTTP_SERVER}/lab5/welcome`} className="list-group-item">
           Welcome
         </a>
-      </div><hr />
+      </div>
+      <hr />
       <EnvironmentVariables />
       <PathParameters />
       <QueryParameters />
       <WorkingWithObjects />
       <WorkingWithArrays />
+      <HttpClient />
+      <WorkingWithObjectsAsynchronously />
+      <WorkingWithArraysAsynchronously />
     </div>
-);}
+  );
+}
