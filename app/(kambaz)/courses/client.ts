f24df792d44781a49ca2bf295ba2e9ac508492e3
reesecantu <65140498+reesecantu@users.noreply.github.com>
2026-04-23 22:52:19 -0400

    connect frontend to backend

--- app/(kambaz)/courses/client.ts
+++ app/(kambaz)/courses/client.ts
@@ -105,3 +105,31 @@ export const unenrollFromCourse = async (userId: string, courseId: string) => {
   );
   return data;
 };
+
+
+const QUIZZES_API = `${HTTP_SERVER}/api/quizzes`;
+
+export const findQuizzesForCourse = async (courseId: string) => {
+  const { data } = await axiosWithCredentials.get(`${COURSES_API}/${courseId}/quizzes`);
+  return data;
+};
+
+export const findQuizById = async (quizId: string) => {
+  const { data } = await axiosWithCredentials.get(`${QUIZZES_API}/${quizId}`);
+  return data;
+};
+
+export const createQuizForCourse = async (courseId: string, quiz: any) => {
+  const { data } = await axiosWithCredentials.post(`${COURSES_API}/${courseId}/quizzes`, quiz);
+  return data;
+};
+
+export const updateQuiz = async (quiz: any) => {
+  const { data } = await axiosWithCredentials.put(`${QUIZZES_API}/${quiz._id}`, quiz);
+  return data;
+};
+
+export const deleteQuiz = async (quizId: string) => {
+  const { data } = await axiosWithCredentials.delete(`${QUIZZES_API}/${quizId}`);
+  return data;
+};
